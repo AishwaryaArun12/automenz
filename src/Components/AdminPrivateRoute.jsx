49eb3f89954677159a0625fec2d23d39e4c7b55d
AishwaryaArun12 <jj4245888@gmail.com>
2024-07-27 21:32:53 +0530

    removed icons

--- src/Components/AdminPrivateRoute.jsx
+++ src/Components/AdminPrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const AdminPrivateRoute = () => {
-  const token = JSON.parse(localStorage.getItem("token"));
+  const token = localStorage.getItem("token");
   return token ? <Outlet /> : <Navigate to="/login" />;
 };
 
